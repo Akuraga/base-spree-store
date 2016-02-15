e11ccd2a5859f130d7ff998f56780ab113966fbf
Lorcan O'Flynn <lorcan.oflynn@gmail.com>
2016-02-15 23:27:04 +1100

    Adding secure variable docker trigger token

--- docker_trigger.sh
+++ docker_trigger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/lorcanoeire/base-spree-store/trigger/10822872-261f-4621-a617-d4ddb0bf6587/
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/lorcanoeire/base-spree-store/trigger/$1/
