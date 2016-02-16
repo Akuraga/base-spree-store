f1f0612c6844b3901955d22b2ca815a9d98541c1
Lorcan O'Flynn <lorcan.oflynn@gmail.com>
2016-02-16 19:12:03 +1100

    Making the docker trigger script a little smarter

--- scripts/docker_trigger.sh
+++ scripts/docker_trigger.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/lorcanoeire/base-spree-store/trigger/$1/
+if [ -z "$1" ]
+  then
+    echo "No TRAVIS_BRANCH argument supplied"
+    exit 1
+else
+  TRAVIS_BRANCH=$1
+fi
+
+if [ -z "$2" ]
+  then
+    echo "No DOCKER_TRIGGER_TOKEN argument supplied"
+    exit 1
+else
+  DOCKER_TRIGGER_TOKEN=$2
+fi
+
+echo "Triggering build for branch: $TRAVIS_BRANCH"
+curl -H "Content-Type: application/json" --data "{"source_type": "Branch", "source_name": "$TRAVIS_BRANCH" }" -X POST https://registry.hub.docker.com/u/lorcanoeire/base-spree-store/trigger/$DOCKER_TRIGGER_TOKEN/
